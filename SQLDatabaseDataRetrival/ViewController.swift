e014fc22f451ed26b8e44bdcb9703d13dd27d0e0
Nikhil Patil <nikhilpatil@macbook-air.local>
2019-03-19 10:09:15 +0530

    Initial Commit

---
+++ SQLDatabaseDataRetrival/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SQLDatabaseDataRetrival
+//
+//  Created by Nikhil Patil on 19/03/19.
+//  Copyright © 2019 Nikhil Patil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
